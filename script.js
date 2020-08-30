e3259cb95b35a2f67ac5c5208306bbcf8feb1daf
rowenarono95 <rowenarono@gmail.com>
2020-08-31 01:07:40 +0300

    styled the placeholder to transparent

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $(".icon").click(function () {
+        $(".design").toggle();
+        $(".design1").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".icon1").click(function () {
+        $(".develop").toggle();
+        $(".develop1").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $(".icon2").click(function () {
+        $(".management").toggle();
+        $(".management1").toggle();
+    });
+});
