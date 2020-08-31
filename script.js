48bf7e01f69ad84182866e5ea0590308eb5b57d5
rowenarono95 <rowenarono@gmail.com>
2020-08-31 13:02:57 +0300

    styled header five

--- script.js
+++ script.js
@@ -18,3 +18,13 @@ $(document).ready(function () {
         $(".management1").toggle();
     });
 });
+
+function myFunction(){
+    var name =document.getElementById("name").value;
+    var email =document.getElementById("email").value;
+    var text =document.getElementById("textArea").value;
+    if(name==""|| email==""||textArea==""){
+      alert("enter your name, email and your message")
+    }
+    else alert(name+" Thank you for reaching out to us")
+  }
